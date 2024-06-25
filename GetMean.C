cf452d07914fbb303f960cdd8d30299dedcedb99
Collin Zheng <61294685+CollinKa@users.noreply.github.com>
2024-06-25 16:17:59 +0900

    save the progress

--- GetMean.C
+++ GetMean.C
@@ -12,7 +12,19 @@
 // Vector to store histogram pointers
 //std::vector<TH1F*> B1CH1_histograms;
 // Vector to store the points
-std::vector<std::pair<double, double>> points;
+std::vector<std::pair<double, double>> points;   // data from CH0
+std::vector<std::pair<double, double>> Ch1Data; 
+
+
+//deal with this later
+/*
+double GetMeanValue(TFile* f_in, TTree* tree , int ChanNum){
+    f_in->GetObject(Form("ch%i/ped_ch%i",ChanNum,ChanNum), tree);
+    TH1F* ch0_h = new TH1F(Form("ch%i_h", ChanNum), "ABEmean", 500, 0, 5000);
+    tree->Draw("ABEmean>>Form("ch%i_h", ChanNum)"); // I doubt if this can work
+}
+*/
+
 
 
 void GetMeanF(int runNum = 1){
@@ -32,6 +44,24 @@ void GetMeanF(int runNum = 1){
 
     //B1CH1_histograms.push_back(ch0_h);
     points.push_back(std::make_pair(runNum, mean_value));
+
+
+
+    //loading the data from Ch1
+    f_in->GetObject("ch1/ped_ch1", tree0);
+    
+    TH1F* ch1_h = new TH1F("ch1_h", "ABEmean", 500, 0, 5000);
+    tree0->Draw("ABEmean>>ch1_h");
+    float mean_value = ch0_h->GetMean();
+    std::cout << mean_value << std::endl;
+
+    //B1CH1_histograms.push_back(ch0_h);
+    points.push_back(std::make_pair(runNum, mean_value));
+
+
+
+
+    
 
 }
 
@@ -61,6 +91,7 @@ void GetMean(){
     }
     // Create a TGraph with the data points
     TGraph *graph = new TGraph(nPoints, x, y);
+    graph->SetMarkerStyle(20); // Set marker style if needed
     graph->Draw("AP");
 
 }
